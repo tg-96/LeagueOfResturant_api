23f04c7453dcaf60b9d6c88ae69c083d4e8cf3b2
정태규 <jtg960528@gmail.com>
2023-11-22 16:24:49 +0900

    init

---
+++ LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/LeagueOfRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.leagueofrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueOfRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeagueOfRestaurantApplication.class, args);
+    }
+
+}
---
+++ LeagueOfRestaurant/src/test/java/com/leagueofrestaurant/LeagueOfRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leagueofrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueOfRestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
