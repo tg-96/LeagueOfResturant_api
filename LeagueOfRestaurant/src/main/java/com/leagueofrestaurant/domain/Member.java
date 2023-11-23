bc6f6e76cc72abf1292227d5d515c17365999759
정태규 <jtg960528@gmail.com>
2023-11-24 00:19:27 +0900

    member entity 수정

--- LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/domain/Member.java
+++ LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/domain/Member.java
@@ -1,11 +1,14 @@
 package com.leagueofrestaurant.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
 public class Member {
     @GeneratedValue
     @Id
@@ -20,4 +23,28 @@ public class Member {
     private LocalDate birthday;
     @Enumerated(EnumType.STRING)
     private MemberType type;
+
+    public Member(String name, String phoneNumber, String password,
+                  Gender gender, LocalDate birthday, MemberType type) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.type = type;
+    }
+
+    /**
+     * 유저 정보 변경
+     */
+    public void changeName(String name){
+        this.name =name;
+    }
+    public void changePhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void changePassword(String password){
+        this.password = password;
+    }
 }
