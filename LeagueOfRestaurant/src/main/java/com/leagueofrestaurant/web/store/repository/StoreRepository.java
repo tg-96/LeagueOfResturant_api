2e29798906632182dc200f80be0b0e0c6a3d38e0
정태규 <jtg960528@gmail.com>
2023-11-27 16:51:04 +0900

    storeRepo 중간완성

--- LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/web/store/repository/StoreRepository.java
+++ LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/web/store/repository/StoreRepository.java
@@ -2,9 +2,16 @@ package com.leagueofrestaurant.web.store.repository;
 
 import com.leagueofrestaurant.web.store.domain.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 
-public interface StoreRepository extends JpaRepository<Store,Long> {
 
-}
+public interface StoreRepository extends JpaRepository<Store,Long>,StoreRepositoryCustom, QuerydslPredicateExecutor {
+    @Override
+    Optional<Store> findById(Long storeId);
+    @Override
+    List<Store> findAll();
+    }
