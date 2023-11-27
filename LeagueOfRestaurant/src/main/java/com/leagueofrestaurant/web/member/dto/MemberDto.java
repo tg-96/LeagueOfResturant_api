38ee54766ac822bbd189a12048a15c97983a0738
정태규 <jtg960528@gmail.com>
2023-11-27 15:27:17 +0900

    MemberDto 생성

---
+++ LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/web/member/dto/MemberDto.java
@@ -0,0 +1,22 @@
+package com.leagueofrestaurant.web.member.dto;
+
+import com.leagueofrestaurant.web.member.domain.Gender;
+
+import java.time.LocalDate;
+
+public class MemberDto {
+    private String name;
+    private String phoneNumber;
+    private String password;
+    private Gender gender;
+    private LocalDate birthday;
+
+    public MemberDto(String name, String phoneNumber, String password, Gender gender, LocalDate birthday) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.gender = gender;
+        this.birthday = birthday;
+    }
+
+}
