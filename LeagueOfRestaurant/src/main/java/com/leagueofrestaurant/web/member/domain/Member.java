e704524f6939d8b0a44864e94b0e0270f54ce5d2
정태규 <jtg960528@gmail.com>
2023-11-27 14:13:31 +0900

    member-repo 기능 테스트 완료

--- LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/web/member/domain/Member.java
+++ LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/web/member/domain/Member.java
@@ -1,10 +1,13 @@
 package com.leagueofrestaurant.web.member.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
@@ -22,17 +25,17 @@ public class Member {
     @NotBlank(message = "비밀번호를 작성하세요.")
     private String password;
 
-    @NotBlank(message = "성별을 입력하세요.")
+    @NotNull(message = "성별을 입력하세요.")
     @Enumerated(EnumType.STRING)
     private Gender gender;
-    @NotBlank(message = "생일을 입력하세요.")
+    @NotNull(message = "생일을 입력하세요.")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
-    @NotBlank(message = "member type 을 입력하세요.")
+    @NotNull(message = "member type 을 입력하세요.")
     @Enumerated(EnumType.STRING)
     private MemberType type;
 
-    public Member( String name, String phoneNumber, String password,
+    public Member(String name, String phoneNumber, String password,
                   Gender gender, LocalDate birthday, MemberType type) {
         this.name = name;
         this.phoneNumber = phoneNumber;
@@ -45,14 +48,15 @@ public class Member {
     /**
      * 유저 정보 변경
      */
-    public void changeName(String name){
-        this.name =name;
+    public void changeName(String name) {
+        this.name = name;
     }
-    public void changePhoneNumber(String phoneNumber){
+
+    public void changePhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
-    public void changePassword(String password){
+    public void changePassword(String password) {
         this.password = password;
     }
 }
--- LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/web/member/repository/MemberRepository.java
+++ LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/web/member/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package com.leagueofrestaurant.web.member.repository;
 import com.leagueofrestaurant.web.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
