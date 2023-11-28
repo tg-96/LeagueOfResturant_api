3dc9ed621bae28cfec2637020b53061906006549
gyujh630 <rbwjrbxo@ajou.ac.kr>
2023-11-29 00:53:27 +0900

    Comment: Review controller 주석 추가

--- LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/web/review/controller/ReviewController.java
+++ LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/web/review/controller/ReviewController.java
@@ -16,16 +16,19 @@ import java.util.List;
 public class ReviewController {
     private final ReviewService reviewService;
 
+    //모든 리뷰 조회
     @GetMapping("/")
     public List<ReviewContent> getAllReviews() {
         return reviewService.getAllReviews();
     }
 
+    //특정 사용자의 리뷰 조회
     @GetMapping("/member/{memberId}")
     public List<ReviewContent> getReviewsByMemberId(@PathVariable Long memberId) {
         return reviewService.getReviewsByMemberId(memberId);
     }
 
+    //특정 가게의 리뷰 조회
     @GetMapping("/store/{storeId}")
     public List<ReviewContent> getReviewsByStoreId(@PathVariable Long storeId) {
         return reviewService.getReviewsByStoreId(storeId);
