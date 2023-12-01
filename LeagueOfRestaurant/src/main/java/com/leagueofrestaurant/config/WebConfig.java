3483b5ace4f979317c5cbb45a78e24f1cb055576
정태규 <jtg960528@gmail.com>
2023-12-02 00:06:52 +0900

    CORS

--- LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/config/WebConfig.java
+++ LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/config/WebConfig.java
@@ -11,12 +11,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://15.165.26.32:8080",
-                        "http://localhost:3000",
-                        "https://localhost:3000",
-                        "https://127.0.0.1:3000"
-                )
-                .allowedHeaders("Origin", "Content-Type", "Authorization")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*").
+                allowCredentials(true);
+
     }
+
 }
