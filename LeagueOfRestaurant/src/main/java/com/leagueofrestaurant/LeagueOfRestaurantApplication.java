d0c338cd280657c9e50e4e1489d0f6796f770581
정태규 <jtg960528@gmail.com>
2023-12-01 23:02:56 +0900

    CORS

--- LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/LeagueOfRestaurantApplication.java
+++ LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/LeagueOfRestaurantApplication.java
@@ -1,11 +1,14 @@
 package com.leagueofrestaurant;
 
+import com.leagueofrestaurant.config.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@Import(WebConfig.class)
 public class LeagueOfRestaurantApplication {
 
     public static void main(String[] args) {
