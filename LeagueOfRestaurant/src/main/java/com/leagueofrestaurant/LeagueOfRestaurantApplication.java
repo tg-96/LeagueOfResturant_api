897949538b9ea7d17fee3d904c718a43dead0870
정태규 <jtg960528@gmail.com>
2023-12-02 02:04:16 +0900

    cors

--- LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/LeagueOfRestaurantApplication.java
+++ LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/LeagueOfRestaurantApplication.java
@@ -1,6 +1,5 @@
 package com.leagueofrestaurant;
 
-import com.leagueofrestaurant.config.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
