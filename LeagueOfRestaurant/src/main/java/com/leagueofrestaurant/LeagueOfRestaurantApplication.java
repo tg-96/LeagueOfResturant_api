fcc91ce97af04a5ee8d89eb2bb075f9324dbef28
정태규 <jtg960528@gmail.com>
2023-12-02 04:06:35 +0900

    cors

--- LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/LeagueOfRestaurantApplication.java
+++ LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/LeagueOfRestaurantApplication.java
@@ -2,8 +2,11 @@ package com.leagueofrestaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -14,4 +17,13 @@ public class LeagueOfRestaurantApplication {
         SpringApplication.run(LeagueOfRestaurantApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
--- LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/config/WebConfig.java
+++ LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/config/WebConfig.java
@@ -1,24 +1,29 @@
-package com.leagueofrestaurant.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-@Configuration
-public class WebConfig {
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);  // json 서버 응답을 자바스크립트에서 처리할 수 있게 해줌
-        config.addAllowedOriginPattern("");      // 모든 ip에 응답을 허용
-        config.addAllowedMethod("");       // 모든 HTTP METHOD 에 허용
-        config.addAllowedHeader("*");      // 모든 HTTP HEADER 에 허용
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(source);
-    }
-}
+//package com.leagueofrestaurant.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.cors().and().csrf().disable();
+//    }
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.addAllowedOrigin("*");
+//        configuration.addAllowedMethod("*");
+//        configuration.addAllowedHeader("*");
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+//}
