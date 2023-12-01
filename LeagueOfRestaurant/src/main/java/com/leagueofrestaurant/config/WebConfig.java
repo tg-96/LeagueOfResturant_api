0adec85ffc36596b3fc8f8f367380e86b69cf54a
정태규 <jtg960528@gmail.com>
2023-12-01 23:20:00 +0900

    CORS

--- LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/config/WebConfig.java
+++ LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/config/WebConfig.java
@@ -10,6 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("http://121.169.26.214:3000",
+                        "http://192.168.0.29:3000");
     }
 }
