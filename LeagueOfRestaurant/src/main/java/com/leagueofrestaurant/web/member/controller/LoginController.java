f995edde99806c74d814bbb26e8631c7effca9c8
정태규 <jtg960528@gmail.com>
2023-12-03 16:27:04 +0900

    set cookie

--- LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/web/member/controller/LoginController.java
+++ LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/web/member/controller/LoginController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
@@ -22,8 +24,10 @@ import static com.leagueofrestaurant.web.common.SessionKey.LOGIN_SESSION_KEY;
 public class LoginController {
     private final MemberService memberService;
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestBody @Valid LoginReq loginReq, HttpSession session){
+    public ResponseEntity<Void> login(@RequestBody @Valid LoginReq loginReq, HttpSession session, HttpServletResponse response){
         memberService.login(loginReq,session);
+        Cookie sessionCookie = new Cookie(LOGIN_SESSION_KEY, session.getId());
+        response.addCookie(sessionCookie);
         return ResponseEntity.ok().header(LOGIN_SESSION_KEY, session.getId()).build();
     }
 
@@ -34,8 +38,10 @@ public class LoginController {
     }
 
     @PostMapping("/join")
-    public ResponseEntity<Void> join(@RequestBody @Valid JoinReq joinReq,HttpSession session){
+    public ResponseEntity<Void> join(@RequestBody @Valid JoinReq joinReq,HttpSession session,HttpServletResponse response){
         memberService.join(joinReq,session);
+        Cookie sessionCookie = new Cookie(LOGIN_SESSION_KEY, session.getId());
+        response.addCookie(sessionCookie);
         return ResponseEntity.ok().header(LOGIN_SESSION_KEY, session.getId()).build();
     }
 
