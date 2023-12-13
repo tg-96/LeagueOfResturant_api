41eef4c36f8d9c85c23a1d84eb8374478018d37a
정태규 <jtg960528@gmail.com>
2023-12-13 17:41:30 +0900

    image upload path mod

--- LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/web/common/ImageService.java
+++ LeagueOfRestaurant/src/main/java/com/leagueofrestaurant/web/common/ImageService.java
@@ -1,11 +1,11 @@
 package com.leagueofrestaurant.web.common;
 
 import com.leagueofrestaurant.api.ocr.OcrService;
-import org.apache.tomcat.util.http.parser.Cookie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -21,6 +21,8 @@ public class ImageService {
     private String uploadPath;
 
     public String saveImage(MultipartFile image) throws IOException {
+        System.out.println("*****************************************************");
+        System.out.println("uploadpath: " + uploadPath);
         String fileName = UUID.randomUUID().toString();
         String format = ocrService.getImageFormat(image);
         Path filePath = Paths.get(uploadPath, fileName + "." + format);
